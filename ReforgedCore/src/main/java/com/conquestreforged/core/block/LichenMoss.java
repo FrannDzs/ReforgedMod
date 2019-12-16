4c256b62a59adb06fbbca6860cc3f2411bee5c0d
HIPPOKLEIDES <pawilkinson20@gmail.com>
2019-12-15 23:10:11 -0800

    lichen and updated reforgedblocks

--- ReforgedCore/src/main/java/com/conquestreforged/core/block/LichenMoss.java
+++ ReforgedCore/src/main/java/com/conquestreforged/core/block/LichenMoss.java
@@ -3,19 +3,88 @@ package com.conquestreforged.core.block;
 import com.conquestreforged.core.asset.annotation.Assets;
 import com.conquestreforged.core.asset.annotation.Model;
 import com.conquestreforged.core.asset.annotation.State;
+import com.conquestreforged.core.block.properties.Waterloggable;
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.BushBlock;
+import net.minecraft.block.SixWayBlock;
+import net.minecraft.fluid.Fluids;
+import net.minecraft.fluid.IFluidState;
+import net.minecraft.item.BlockItemUseContext;
+import net.minecraft.state.BooleanProperty;
+import net.minecraft.state.StateContainer;
+import net.minecraft.state.properties.BlockStateProperties;
+import net.minecraft.util.Direction;
+import net.minecraft.util.Util;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.IBlockReader;
+import net.minecraft.world.IWorld;
+
+import java.util.Map;
 
 @Assets(
-        state = @State(name = "%s", template = "parent_plant_dense"),
+        state = @State(name = "%s", template = "parent_lichen"),
         item = @Model(name = "item/%s", parent = "block/%s_pane_ns", template = "item/parent_round_arch"),
         block = {
-                @Model(name = "block/%s_plant_dense_model_1", template = "block/parent_plant_dense_model_1"),
-                @Model(name = "block/%s_plant_dense_model_2", template = "block/parent_plant_dense_model_2"),
-                @Model(name = "block/%s_plant_dense_model_3", template = "block/parent_plant_dense_model_3"),
+                @Model(name = "block/%s_lichen", template = "block/parent_lichen"),
+                @Model(name = "block/%s_lichen_n", template = "block/parent_lichen_n"),
+                @Model(name = "block/%s_lichen_ne", template = "block/parent_lichen_ne"),
+                @Model(name = "block/%s_lichen_ns", template = "block/parent_lichen_ns"),
+                @Model(name = "block/%s_lichen_nse", template = "block/parent_lichen_nse"),
+                @Model(name = "block/%s_lichen_nsew", template = "block/parent_lichen_nsew"),
                 @Model(name = "block/%s_pane_ns", template = "block/parent_flatpane_ns"),
         }
 )
-public class LichenMoss extends Bush {
+public class LichenMoss extends BushBlock implements Waterloggable {
+    public static final BooleanProperty NORTH = SixWayBlock.NORTH;
+    public static final BooleanProperty EAST = SixWayBlock.EAST;
+    public static final BooleanProperty SOUTH = SixWayBlock.SOUTH;
+    public static final BooleanProperty WEST = SixWayBlock.WEST;
+    protected static final Map<Direction, BooleanProperty> FACING_TO_PROPERTY_MAP = SixWayBlock.FACING_TO_PROPERTY_MAP.entrySet().stream().filter((p_199775_0_) -> {
+        return p_199775_0_.getKey().getAxis().isHorizontal();
+    }).collect(Util.toMapCollector());
     public LichenMoss(Properties properties) {
         super(properties);
+        this.setDefaultState(this.stateContainer.getBaseState().with(WATERLOGGED, false));
+    }
+
+    @Override
+    public BlockState getStateForPlacement(BlockItemUseContext context) {
+        IBlockReader iblockreader = context.getWorld();
+        BlockPos blockpos = context.getPos();
+        IFluidState ifluidstate = context.getWorld().getFluidState(context.getPos());
+        BlockPos blockpos1 = blockpos.north();
+        BlockPos blockpos2 = blockpos.east();
+        BlockPos blockpos3 = blockpos.south();
+        BlockPos blockpos4 = blockpos.west();
+        BlockState blockstate = iblockreader.getBlockState(blockpos1);
+        BlockState blockstate1 = iblockreader.getBlockState(blockpos2);
+        BlockState blockstate2 = iblockreader.getBlockState(blockpos3);
+        BlockState blockstate3 = iblockreader.getBlockState(blockpos4);
+        return super.getStateForPlacement(context).with(NORTH, blockstate.func_224755_d(iblockreader, blockpos1, Direction.SOUTH)).with(EAST, blockstate1.func_224755_d(iblockreader, blockpos1, Direction.WEST)).with(SOUTH, blockstate2.func_224755_d(iblockreader, blockpos3, Direction.NORTH)).with(WEST, blockstate3.func_224755_d(iblockreader, blockpos4, Direction.EAST)).with(WATERLOGGED, Boolean.valueOf(ifluidstate.getFluid() == Fluids.WATER));
+    }
+
+    @Override
+    protected boolean isValidGround(BlockState state, IBlockReader worldIn, BlockPos pos) {
+        return true;
+    }
+
+    @Override
+    public BlockState updatePostPlacement(BlockState stateIn, Direction facing, BlockState facingState, IWorld worldIn, BlockPos currentPos, BlockPos facingPos) {
+        if (stateIn.get(WATERLOGGED)) {
+            worldIn.getPendingFluidTicks().scheduleTick(currentPos, Fluids.WATER, Fluids.WATER.getTickRate(worldIn));
+        }
+
+        return facing.getAxis().getPlane() == Direction.Plane.HORIZONTAL ? stateIn.with(FACING_TO_PROPERTY_MAP.get(facing), facingState.func_224755_d(worldIn, facingPos, facing.getOpposite())) : super.updatePostPlacement(stateIn, facing, facingState, worldIn, currentPos, facingPos);
+    }
+
+    @Override
+    public IFluidState getFluidState(BlockState state) {
+        return Waterloggable.getFluidState(state);
+    }
+
+    @Override
+    protected void fillStateContainer(StateContainer.Builder<Block, BlockState> builder) {
+        builder.add(NORTH, EAST, WEST, SOUTH, WATERLOGGED);
     }
 }
