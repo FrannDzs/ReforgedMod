0c2884249d7a6f645ad2e13afeec8075748235fb
HIPPOKLEIDES <pawilkinson20@gmail.com>
2021-04-25 10:46:21 -0400

    more item models and switch order of dependency+intro screen

--- ReforgedClient/src/main/java/com/conquestreforged/client/tutorial/TutorialRenderEvent.java
+++ ReforgedClient/src/main/java/com/conquestreforged/client/tutorial/TutorialRenderEvent.java
@@ -42,9 +42,9 @@ public class TutorialRenderEvent {
     public void render(GuiScreenEvent.InitGuiEvent.Post event) {
         if (event.getGui() instanceof MainMenuScreen) {
             List<Dependency> missing = dependencies.getMissingDependencies();
-            DependencyScreen screen2 = new DependencyScreen(event.getGui(), section, missing);
-            IntroScreen screen1 = new IntroScreen(screen2, section);
-            IntroScreen screen3 = new IntroScreen(event.getGui(), section);
+            IntroScreen introScreen = new IntroScreen(event.getGui(), section);
+            DependencyScreen dependencyScreen = new DependencyScreen(event.getGui(), section, missing);
+            DependencyScreen dependencyScreen2 = new DependencyScreen(introScreen, section, missing);
 
             if ((section.getOrElse("ignore_dependencies", false) && section.getOrElse("ignore_intro", false))
                     || missing.isEmpty() && Tutorials.intro
@@ -54,16 +54,16 @@ public class TutorialRenderEvent {
                 return;
             }
 
-            if (!Tutorials.dependencies) {
-                if (section.getOrElse("ignore_dependencies", false) || missing.isEmpty()) {
-                    Minecraft.getInstance().displayGuiScreen(screen3);
-                    if (Tutorials.intro) {
+            if (!Tutorials.intro) {
+                if (section.getOrElse("ignore_intro", false)) {
+                    Minecraft.getInstance().displayGuiScreen(dependencyScreen);
+                    if (Tutorials.dependencies) {
                         MinecraftForge.EVENT_BUS.unregister(this);
                     }
-                } else if (section.getOrElse("ignore_intro", false)) {
-                    Minecraft.getInstance().displayGuiScreen(screen2);
+                } else if (section.getOrElse("ignore_dependencies", false) || missing.isEmpty()) {
+                    Minecraft.getInstance().displayGuiScreen(introScreen);
                 } else {
-                    Minecraft.getInstance().displayGuiScreen(screen1);
+                    Minecraft.getInstance().displayGuiScreen(dependencyScreen2);
                 }
             }
         }
